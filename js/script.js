a2cb383997b3f4f33eccc20c1a1d7b5c3dc713aa
Monivillagra <villagra.ramirez@gmail.com>
2016-11-09 00:19:02 -0300

    agregando carpetas

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// function añade(){
+// 	document.getElementById('fname').innerHTML = creaLista();
+// }
+
+// function creaLista() {
+//     var x = document.getElementById("fname");
+//     x.value = x.value.toUpperCase();
+// }
+
+
